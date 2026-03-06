c8e8d05d03c3d54b41acfe627ed2c876cfa46f28
rehaan299 <rehaan@ualberta.ca>
2026-03-06 15:48:54 -0700

    test should again fail as countcities not defined

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -30,5 +30,14 @@ public class CustomListTest {
             list.deleteCity(calgary);
         });
     }
-
+    //countcities not defined yet
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+        list.addCity(new City("Calgary", "AB"));
+        assertEquals(1, list.countCities());
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(2, list.countCities());
+    }
 }
