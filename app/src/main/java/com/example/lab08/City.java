69a2206139275d9cf0427f937737c298c041778d
rehaan299 <rehaan@ualberta.ca>
2026-03-06 15:28:11 -0700

    adding small comment for github integration

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -4,6 +4,8 @@ public class City {
     private String city;
     private String province;
 
+//    adding a comment for github integration
+
     public City(String city, String province){
         this.city = city;
         this.province = province;
